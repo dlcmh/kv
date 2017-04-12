ca2cfb387016122e5edd0924dc7e1f5f75ba8e51
David Chin <dlcmhd@me.com>
2017-04-12 21:22:21 +0800

    Test setup with ExUnit callbacks

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,8 +1,15 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores value by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket} # ExUnit will merge the second element of the tuple,
+    # a keyword list, into the test context, a map
+  end
+
+  test "stores value by key", %{bucket: bucket} do
+    # {:ok, bucket} = KV.Bucket.start_link
+    # With ExUnit callbacks in place, `bucket` is now the bucket from the setup macro
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
